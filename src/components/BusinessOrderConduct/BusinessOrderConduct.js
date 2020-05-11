b07205894d3657eadd7266b706eed85d30419817
zuopengyu <zuopengyu2017@qq.com>
2020-05-11 19:57:18 +0800

    商家中心支付方式显示+撤单修改

--- src/components/BusinessOrderConduct/BusinessOrderConduct.js
+++ src/components/BusinessOrderConduct/BusinessOrderConduct.js
@@ -69,29 +69,29 @@ root.computed.result_socket = function () {
 root.watch = {}
 
 root.watch.result_socket = function (newValue, oldValue) {
-  let user_id = newValue.data.userId;
-  if (user_id != this.userId || newValue.key != 'cancelPostersOrder') return;
-  // console.log('watchSocket', newValue)
-  if (newValue.data.result == 0) {
-    this.popOpen = true
-    this.popType = 1
-    this.popText = '撤单成功'
-    this.cancelBuyBoxFlag = false
-    this.ajaxFlag = false
-    this.getPartPosterOrderList()
-    this.isSocket = false
-    this.st = setTimeout(()=>{
-    this.$router.go(0)},2000)
-    return
-  }
-  if (newValue.data.result != 0) {
-    this.popOpen = true;
-    this.popType = 0
-    this.popText = newValue.data.message;
-    this.ajaxFlag = false
-    this.st = setTimeout(()=>{
-    this.$router.go(0)},2000)
-  }
+  // let user_id = newValue.data.userId;
+  // if (user_id != this.userId || newValue.key != 'cancelPostersOrder') return;
+  // // console.log('watchSocket', newValue)
+  // if (newValue.data.result == 0) {
+  //   this.popOpen = true
+  //   this.popType = 1
+  //   this.popText = '撤单成功'
+  //   this.cancelBuyBoxFlag = false
+  //   this.ajaxFlag = false
+  //   this.getPartPosterOrderList()
+  //   this.isSocket = false
+  //   this.st = setTimeout(()=>{
+  //   this.$router.go(0)},2000)
+  //   return
+  // }
+  // if (newValue.data.result != 0) {
+  //   this.popOpen = true;
+  //   this.popType = 0
+  //   this.popText = newValue.data.message;
+  //   this.ajaxFlag = false
+  //   this.st = setTimeout(()=>{
+  //   this.$router.go(0)},2000)
+  // }
 }
 
 
@@ -148,6 +148,26 @@ root.methods.getPartPosterOrderList = function () {
 }
 
 root.methods.openCancelBox = function (item) {
+
+  let frozenVolume = item.frozenVolume;//冻结数量
+
+  if(frozenVolume > 0){
+    this.popOpen = true
+    this.popType = 0
+    this.popText = '存在未完成订单,无法撤单'
+    return
+  }
+  /*let volume = item.volume;//全部数量
+  let tradingVolume = item.tradingVolume;//已成交数量
+  let feeRate = item.feeRate || 0.001;//费率
+  let feeAccuracy = ((feeRate || '.') + '').split('.')[1].length//和''拼接的目的是变成字符串
+
+  //可撤回数量 = 全部数量 - 成交数量 * (1+费率)
+  let withdrawable = this.accMinus(volume,
+    this.accMul(tradingVolume,this.accAdd(1,feeRate))
+  )
+
+  item.withdrawable =this.toFixed(withdrawable,(feeAccuracy || 0)+2)*/
   this.itemContainer = item
 
   // console.log('itemContainer', this.itemContainer)
@@ -176,15 +196,47 @@ root.methods.submitCancelPosterOrder = function () {
     .then(({data}) => {
       typeof data === 'string' && (data = JSON.parse(data))
       this.cancelBuyBoxFlag = true
+
+      if (data.result === 'FAIL' || data.code) {
+        // 1015 未找到相关广告单
+        // 1033 用户正在下单无法撤单
+        // 1036 存在未完成订单,无法下架
+        // 1034 广告下架失败
+        switch (data.code) {
+          case 1015://未找到相关广告单
+            this.popOpen = true
+            this.popType = 0
+            this.popText = '未找到相关广告单'
+            break;
+          case 1033://用户正在下单无法撤单
+            this.popOpen = true
+            this.popType = 0
+            this.popText = '用户正在下单无法撤单'
+            break;
+          case 1036://存在未完成订单,无法下架
+            this.popOpen = true
+            this.popType = 0
+            this.popText = '存在未完成订单,无法撤单'
+            break;
+          case 1034://广告下架失败
+            this.popOpen = true
+            this.popType = 0
+            this.popText = '撤单失败'
+            break;
+        }
+        return
+      }
+
       this.popOpen = true
       this.popType = 1
       this.popText = '已提交'
       this.cancelBuyBoxFlag = false
       // window.location.reload()
       this.st = setTimeout(()=>{
-      if(this.isSocket){
-      this.$router.go(0)}
-      },3000)
+      // if(this.isSocket){
+      this.$router.go(0)
+      // }
+      },1000)
     }).catch((err) => {
     console.log('err', err)
     this.cancelBuyBoxFlag = false
