d5f69096211b6a524422a8bc98a0361006d21343
lcc <1398750946@qq.com>
2020-05-13 14:50:33 +0800

    添加认证状态

--- src/components/OrderCancel/OrderCancel.js
+++ src/components/OrderCancel/OrderCancel.js
@@ -25,6 +25,11 @@ root.computed = {};
 root.computed.userId = function () {
   return this.$store.state.authState.userId
 }
+// 认证状态-实名认证
+root.computed.identity = function () {
+  if(this.$store.state.authState.idType !='NONE')return true
+  return false
+}
 root.props = {};
 
 root.created = function () {
@@ -33,6 +38,7 @@ root.created = function () {
 
 	// 获取订单列表
 	this.GET_ORDER_CONDUCT();
+	this.getAuthState()
 };
 
 root.methods = {};
@@ -76,6 +82,35 @@ root.methods.clickChangePage = function (page) {
 root.methods.GO_DETAIL = function (id, orderType) {
 	this.$router.push({name: 'OrderDetails', query: {'type': '3','orderId': id, 'orderType': orderType}});
 }
+// 获取认证状态
+root.methods.getAuthState = function () {
+  if (!this.$store.state.authState) {
+    // this.authStateReady = true
+    // if (this.bindMobile) {
+    //   this.picked = 'bindMobile'
+    // }
+    // if (this.bindGa) {
+    //   this.picked = 'bindGA'
+    // }
+    return
+  }
+  this.$http.send('GET_AUTH_STATE')
+    .then(({data}) => {
+      // console.log(data,'手机认证状态')
+      typeof data === 'string' && (data = JSON.parse(data))
+      if (!data) return
+      this.$store.commit('SET_AUTH_STATE', data.data)
+      // if (data.dataMap.sms) {
+      //   this.picked = 'bindMobile'
+      // }
+      // if (data.dataMap.ga) {
+      //   this.picked = 'bindGA'
+      // }
+      // this.authStateReady = true
+    })
+    .catch(err => {
 
+    })
+}
 
 export default root;
