a8e7955540d84e243117f2d6f1d7df40ef91fd54
sunjiahui <2570167180@qq.com>
2020-05-13 11:22:20 +0800

    订单取消加载更多

--- src/components/OrderCancel/OrderCancel.js
+++ src/components/OrderCancel/OrderCancel.js
@@ -8,9 +8,11 @@ root.data = function () {
 		maxPage: 1,
 		selectIndex: 1,
 
+    page:1,
 		// 列表
 		list: [],
-
+    loadingMoreShow:false,
+    showLoadingMore:true,
 	}
 };
 
@@ -40,24 +42,44 @@ root.methods = {};
 root.methods.GET_ORDER_CONDUCT = function (search) {
 	this.$http.send('GET_LIST_ORDERS', {
     query: {
-			offset: this.selectIndex,
+      page: this.page,
 			maxResults: 10,
 			// status: 'CANCEL', //  “PROCESSING”进行中, “COMPLETE”已完成, “CANCEL”已取消
       status: 4, //   1进行中, 3已完成, 4已取消
       ctcOrderId: search || '',
 		}
 	}).then(({data}) => {
-		if (data.code == 200) {
-			this.loading = false;
-			// let datas = data.dataMap.ctcOrders;
-			let datas = data.data;
-			this.list = datas;
-			if (this.list[0] == null) {
-				this.list = [];
-				return;
-			};
-			this.maxPage = datas.page.totalPages;
-			this.selectIndex = datas.page.pageIndex;
+    typeof data === 'string' && (data = JSON.parse(data))
+    this.loading = false;
+
+    if (data.code == 200) {
+
+      this.list.push(...data.data)
+      //
+      //
+      if (data.data.length < 10) {
+        this.showLoadingMore = false
+      }
+      //
+      //
+      this.page = this.page + 1
+      // this.pageSize = this.pageSize + 1
+      //
+      // this.loading = false
+      this.loadingMoreIng = false
+
+
+      // if (data.code == 200) {
+		// 	this.loading = false;
+		// 	// let datas = data.dataMap.ctcOrders;
+		// 	let datas = data.data;
+		// 	this.list = datas;
+		// 	if (this.list[0] == null) {
+		// 		this.list = [];
+		// 		return;
+		// 	};
+		// 	// this.maxPage = datas.page.totalPages;
+			// this.selectIndex = datas.page.pageIndex;
 		}
 		if (data.code == 1) {
 			window.location.reload();
@@ -67,10 +89,16 @@ root.methods.GET_ORDER_CONDUCT = function (search) {
 	});
 }
 // 切换页码
-root.methods.clickChangePage = function (page) {
-	this.selectIndex = page;
-	// 切换页码
-	this.GET_ORDER_CONDUCT();
+// root.methods.clickChangePage = function (page) {
+// 	this.selectIndex = page;
+// 	// 切换页码
+// 	this.GET_ORDER_CONDUCT();
+// }
+
+// 点击加载更多
+root.methods.clickLoadingMore = function () {
+  // this.loadingMoreIng = true
+  this.GET_ORDER_CONDUCT()
 }
 // 跳转详情
 root.methods.GO_DETAIL = function (id, orderType) {
